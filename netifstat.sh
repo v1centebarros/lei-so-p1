1ff6ead3fc012b636685391aaf358729ccb61a61
Vicente Barros <vmabarros@ua.pt>
2021-11-27 18:07:27 +0000

    UPDATE: -p e -c estão a funcionar corretamente independente da ordem

--- netifstat.sh
+++ netifstat.sh
@@ -1,34 +1,90 @@
 #!/bin/bash
 clear
 
-INTERFACES=$(ip -o link show | awk -F': ' '{print $2}')
-DATA_SIZE=0 #Valor default da tipo de dados (bytes) 1=Kb 2=Mb
-TIME=$1
+INTERFACES=$(ip -o link show | awk -F': ' '{print $2}' | xargs -n1 | sort | xargs)
+DATA_SIZE=-1 #Valor default da tipo de dados (bytes) 1=Kb 2=Mb
+TIME=${@: -1}
+REGEX_STR=-1
+LIST_SIZE=-1
+IS_LOOP=0
 
 function main() {
-    print_table $(echo $INTERFACES | xargs -n1 | sort | xargs) # Inte
-    #validate_args $@
+    validate_args $@
+    data_cleansing
+    print_table $INTERFACES
 }
+
 
 
 function validate_args() {
-    while getopts ':c:' option; do
-    case ${option} in
-    c )
-        print_table ${INTERFACES}
-    ;;
+    while getopts 'c:p:bkml' option; do
+        case ${option} in
+            c )
+                REGEX_STR=${OPTARG}
+            ;;
 
-    :)
-      echo "$0: Must supply an argument to -$OPTARG." >&2
-      exit 1
-    ;;
-    ?)
-      echo "Invalid option: -${OPTARG}."
-      exit 2
-    ;;
-    esac
+            p ) 
+                if [[ ${OPTARG} =~ ^[0-9]+$ ]]; then
+                    LIST_SIZE=${OPTARG}
+                else
+                    echo "ERRO! valor inválido para -p"
+                    exit 1
+                fi
+            ;;
+
+            b )
+                if [ $DATA_SIZE -eq -1 ];then
+                    DATA_SIZE=0
+                else
+                    echo "ERRO! Múltiplos valores foram inseridos para o tamanho da informação"
+                    exit 1
+                fi
+            ;;
+            k ) 
+                if [[ $DATA_SIZE -eq -1 ]];then
+                    DATA_SIZE=1
+                else
+                    echo "ERRO! Múltiplos valores foram inseridos para o tamanho da informação"
+                    exit 1
+                fi
+            ;;
+            m ) 
+                if [[ $DATA_SIZE -eq -1 ]];then
+                    DATA_SIZE=2
+                else
+                    echo "ERRO! Múltiplos valores foram inseridos para o tamanho da informação"
+                    exit 1
+                fi
+            ;;
+        esac
     done
+
 }
+
+
+
+function data_cleansing () {
+    local interfaces_filtered=()
+    if [[ ! $REGEX_STR = -1 ]]; then
+        for int in $INTERFACES; do
+            if [[ $int =~ $REGEX_STR ]]
+            then
+                interfaces_filtered+=($int)
+            fi
+        done
+        INTERFACES=${interfaces_filtered[@]}
+    fi
+    
+
+    if [[ ! $LIST_SIZE = -1 ]]; then
+        INTERFACES=$(cut -d ' ' -f 1-$LIST_SIZE <<< $INTERFACES)
+    fi   
+}
+
+
+
+
+
 
 
 
