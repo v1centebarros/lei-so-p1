96e217dacb6221da6cd2d2c5b21e7e1c736f4eb4
Vicente Barros <vmabarros@ua.pt>
2021-11-27 19:46:05 +0000

    UPDATE: Sort Feito

--- netifstat.sh
+++ netifstat.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
-clear
+#clear
 
 INTERFACES=$(ip -o link show | awk -F': ' '{print $2}' | xargs -n1 | sort | xargs)
 DATA_SIZE=-1 #Valor default da tipo de dados (bytes) 1=Kb 2=Mb
 TIME=${@: -1}
 REGEX_STR=-1
 LIST_SIZE=-1
+SORT_TYPE=-1
 IS_LOOP=0
 
 function main() {
     validate_args $@
     data_cleansing
-    print_table $INTERFACES
+    print_data
 }
 
-
-
 function validate_args() {
-    while getopts 'c:p:bkml' option; do
+    while getopts 'c:p:bkmltrTRv' option; do
         case ${option} in
             c )
                 REGEX_STR=${OPTARG}
@@ -33,35 +32,61 @@ function validate_args() {
             ;;
 
             b )
-                if [ $DATA_SIZE -eq -1 ];then
-                    DATA_SIZE=0
-                else
-                    echo "ERRO! Múltiplos valores foram inseridos para o tamanho da informação"
-                    exit 1
-                fi
+                validate_data_size 0
             ;;
             k ) 
-                if [[ $DATA_SIZE -eq -1 ]];then
-                    DATA_SIZE=1
-                else
-                    echo "ERRO! Múltiplos valores foram inseridos para o tamanho da informação"
-                    exit 1
-                fi
+                validate_data_size 1
             ;;
             m ) 
-                if [[ $DATA_SIZE -eq -1 ]];then
-                    DATA_SIZE=2
-                else
-                    echo "ERRO! Múltiplos valores foram inseridos para o tamanho da informação"
-                    exit 1
-                fi
+                validate_data_size 2
             ;;
+
+            t )
+                validate_sort_type 1
+            ;;
+            
+            r )
+                validate_sort_type 2
+            ;;
+            
+            T )
+                validate_sort_type 3
+            ;;
+            
+            R )
+                validate_sort_type 4
+            ;;
+            
+            v )
+                validate_sort_type 5
+            ;;
+
+
         esac
     done
 
 }
 
+function validate_data_size () {
+        if [[ $DATA_SIZE -eq -1 ]];then
+        DATA_SIZE=$1
+    else
+        echo "ERRO! Múltiplos valores foram inseridos para o tamanho da informação"
+        exit 1
+    fi
 
+}
+
+
+function validate_sort_type () {
+        if [[ $SORT_TYPE -eq -1 ]];then
+        SORT_TYPE=$1
+    else
+        echo "ERRO! Múltiplos valores foram inseridos para o tipo de sort"
+        exit 1
+    fi
+
+}
 
 function data_cleansing () {
     local interfaces_filtered=()
@@ -78,27 +103,24 @@ function data_cleansing () {
 
     if [[ ! $LIST_SIZE = -1 ]]; then
         INTERFACES=$(cut -d ' ' -f 1-$LIST_SIZE <<< $INTERFACES)
-    fi   
+    fi
+
+    if [[ $DATA_SIZE = -1 ]]; then
+        DATA_SIZE=0
+    fi
 }
 
-
-
-
-
-
-
-
 function print_table() {
-    local TABLEHEADER="%-20s %-12s %-12s %-12s %-12s\n"
     local TABLECONTENT="%-20s %-12s %-12s %-12.2f %-12.2f\n"
     local ITF_DATA=()
+
     for i in $@
     do
         ITF_DATA+=($(ifconfig $i | awk '/RX packets /{print $5}'))
         ITF_DATA+=($(ifconfig $i | awk '/TX packets /{print $5}'))
     done
     sleep $TIME    
-    printf "$TABLEHEADER" "NETIF" "TX" "RX" "TRATE" "RRATE"
+    
     
     idx=-1
     for i in $@
@@ -108,6 +130,11 @@ function print_table() {
         #RX do intervalo (final - inicial)
         RX=$(($(ifconfig $i | awk '/TX packets /{print $5}')-${ITF_DATA[$(($idx+2))]}))
 
+        if [[ ! $DATA_SIZE = 0 ]];then
+            TX=$(($TX/1024*$DATA_SIZE))
+            RX=$(($RX/1024*$DATA_SIZE))
+        fi
+
         TXRATE=$(($TX/$TIME))
         RXRATE=$(($RX/$TIME))
         #Como sabemos que queremos apenas o TX e o RX sabemos que ao ir buscar a informação ao vetor é sempre idx+1 ou idx+2
@@ -116,5 +143,44 @@ function print_table() {
     done
 }
 
+
+
+function print_data() {
+    if [[ $IS_LOOP = 0 ]]; then
+        local TABLEHEADER="%-20s %-12s %-12s %-12s %-12s\n"
+        printf "$TABLEHEADER" "NETIF" "TX" "RX" "TRATE" "RRATE"
+        
+        case $SORT_TYPE in
+            -1 ) 
+                print_table $INTERFACES
+            ;;
+            1 )
+                print_table $INTERFACES | sort -k 2 -n -r
+            ;;
+
+            2 )
+                print_table $INTERFACES | sort -k 3 -n -r
+            ;;
+
+            3 )
+                print_table $INTERFACES | sort -k 4 -n -r
+            ;;
+
+            4 )
+                print_table $INTERFACES | sort -k 5 -n -r
+            ;;
+
+            5 )
+                print_table $INTERFACES | sort -r
+            ;;
+        
+        
+        esac
+    else
+        local TABLEHEADER="%-20s %-12s %-12s %-12s %-12s\n"
+        #TODO esta é para o loop
+    fi
+
+}
 
 main $@
